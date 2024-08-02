91aa2910579141106e8e2f9294eadd0f30555353
JENNY-257 <j.iribagiza2@gmail.com>
2024-08-02 11:14:21 +0200

    design signup page

--- src/components/header/NavBar.jsx
+++ src/components/header/NavBar.jsx
@@ -8,8 +8,8 @@ const NavBar = () => {
         <ul className='text-base text-gray-700 pt-10 md:flex md:justify-between md:pt-0 ml-[600px] mt-[30px]'>
             <li><Link className='md:p-6 py-6  hover:text-blue-400 ' to="/">Home</Link></li>
             <li><Link className='md:p-6 py-6  hover:text-blue-400' to="contact">Contact</Link></li>
-            <li><Link className='md:p-6 py-6  hover:text-blue-400' to="login">Login</Link></li>
-            <li><Link className='md:p-6 py-6  hover:text-blue-400' to="signup">Signup</Link></li>
+            <li><Link className='md:p-6 py-6  hover:text-blue-400' to="/login">Login</Link></li>
+            <li><Link className='md:p-6 py-6  hover:text-blue-400' to="/signup">Signup</Link></li>
         </ul>
       </nav>
     </div>
